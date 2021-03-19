a24b792b4b1ef561e57713ef65ddf87f0971aee1
ryowuandjanet <ryowudata@gmail.com>
2021-03-19 17:29:40 +0800

    Initial Commit

---
+++ blog_api/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+  queryset = Post.postobjects.all()
+  serializer_class = PostSerializer
+  pass
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+  pass
